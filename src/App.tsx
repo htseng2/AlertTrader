9cf36099ea357350330a62dd3b347f7001d18952
Christopher Tseng <christopher.tseng@frontier.cool>
2024-11-04 17:33:17 +0800

    feat: integrate TradingView widgets into the app
    - Added `react-ts-tradingview-widgets` as a new dependency.
    - Integrated `AdvancedRealTimeChart` component into `App.tsx` with studies for RSI, MACD, and Bollinger Bands.
    - Updated `package.json` and `package-lock.json` to include the new dependency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  AdvancedRealTimeChart,
+  type Studies,
+} from "react-ts-tradingview-widgets";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const myStudy: Studies[] = [
+    "RSI@tv-basicstudies",
+    "MACD@tv-basicstudies",
+    "BollingerBandsR@tv-basicstudies",
+  ];
 
   return (
     <>
@@ -28,8 +37,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <AdvancedRealTimeChart
+        symbol="FX_IDC:TWDJPY"
+        interval="D"
+        timezone="Asia/Taipei"
+        theme="dark"
+        studies={myStudy}
+        style="1"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
